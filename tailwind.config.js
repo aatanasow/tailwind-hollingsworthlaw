4b14a7bf8ccc97f2b3cc03ccfd6d6d5a3f7d283d
aatanasow <aatanasow@yahoo.co.uk>
2023-10-25 16:23:01 +0300

    Customizing spacing according to the mockup

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,19 @@ module.exports = {
       "5xl": "3.833rem",
       "6xl": "5.056rem",
     },
+    spacing: {
+      0: "0px",
+      1: "8px",
+      2: "16px",
+      3: "24px",
+      4: "32px",
+      5: "40px",
+      6: "48px",
+      7: "56px",
+      8: "64px",
+      9: "72px",
+      10: "80px",
+    },
     extend: {
       colors: {
         "cOrange-10": "#FCFAF6",
